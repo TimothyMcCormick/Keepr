4d065fd76d0cf2b49d141776c72295616cc82aa0
Timothy McCormick <breaking490@gmail.com>
2022-07-29 14:43:17 -0600

    models created

--- Keepr/Models/Account.cs
+++ Keepr/Models/Account.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-
 namespace Keepr.Models
 {
-    public class Account
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Picture { get; set; }
-    }
+  public class Account
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Picture { get; set; }
+  }
+
+
 }
